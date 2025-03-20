ff74214f3dbc668d10cfc3c5e1966ee15b3f7e1c
aimei60 <104590763+aimei60@users.noreply.github.com>
2025-03-20 13:29:06 +0000

    I added a password as I will need a password when I work with terraform later

--- main.py
+++ main.py
@@ -4,13 +4,17 @@ from sqlalchemy import create_engine, MetaData
 from sqlalchemy.orm import Session
 from database import Base, workouts, workout_sections, workoutRoutine
 
-load_dotenv(dotenv_path="sens.env")
+load_dotenv("sens.env")
+
 
 DB_USER = os.environ.get("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
 DB_NAME = os.environ.get("DB_NAME")
 DB_HOST = os.environ.get("DB_HOST")
 DB_PORT = os.environ.get("DB_PORT")
 
-engine = create_engine(f"postgresql+psycopg2://{DB_USER}:@{DB_HOST}:{DB_PORT}/{DB_NAME}", echo=True)
-Base.metadata.create_all(engine)
+engine = create_engine(f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}", echo=True)
+#Base.metadata.create_all(engine)
+
+
 
