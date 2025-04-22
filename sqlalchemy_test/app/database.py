0dacbed6ca789d577fe0d907fec3cf9261e3a4c8
aimei60 <104590763+aimei60@users.noreply.github.com>
2025-04-22 19:39:19 +0100

    Added sqlalchemy engine and session tools. Also added a test database to connect to and run tests and defined my model base class

---
+++ sqlalchemy_test/app/database.py
@@ -0,0 +1,17 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, DeclarativeBase
+import os
+from dotenv import load_dotenv
+
+load_dotenv("sens.env")
+
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+
+engine = create_engine(f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/test_db")
+SessionLocal = sessionmaker(bind=engine)
+
+class Base(DeclarativeBase):
+    pass
