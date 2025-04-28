6cc11473f1aac750cf5739e4ff9283ad17f00310
aimei60 <lizali26@outlook.com>
2025-04-28 10:58:48 +0100

    Renamed my test files, imported my tables into test_models, created my test_db table and added a dummy test to see if its working. I also added all my pycache files into gitignore instead of deleting them everytime.

--- sqlalchemy_test/app/test_database.py
+++ sqlalchemy_test/app/test_database.py
@@ -2,11 +2,12 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
 import os
 from dotenv import load_dotenv
+from urllib.parse import quote_plus
 
 load_dotenv("sens.env")
 
 DB_USER = os.getenv("DB_USER")
-DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_PASSWORD = quote_plus(os.getenv("DB_PASSWORD"))
 DB_HOST = os.getenv("DB_HOST")
 DB_PORT = os.getenv("DB_PORT")
 
@@ -16,4 +17,3 @@ SessionLocal = sessionmaker(bind=engine)
 class Base(DeclarativeBase):
     pass
 
-#run it
--- sqlalchemy_test/app/test_models.py
+++ sqlalchemy_test/app/test_models.py
@@ -1,2 +1,8 @@
-from app.test_database import Base
+from sqlalchemy_test.app.test_database import Base, engine, SessionLocal
+from database import workouts, workout_sections, workoutRoutine, User, UserWorkoutRequest, UserProfile
 
+Base.metadata.create_all(engine)
+
+def test_dummy():
+    assert True
+    
