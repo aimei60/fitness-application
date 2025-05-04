a0362f8cc3eae4d5b78715c0691c4d7147b5b483
aimei60 <lizali26@outlook.com>
2025-05-04 12:35:24 +0100

    added my tests for the second table

--- sqlalchemy_test/app/test_models.py
+++ sqlalchemy_test/app/test_models.py
@@ -35,5 +35,82 @@ def test_insert_invalid_workout_name():
     
     with pytest.raises(IntegrityError):
         session.commit()
-    
+
+    session.rollback()
     session.close()
+    
+def test_data_insertion_retrieval_workouts_sections():
+    session = SessionLocal()
+    
+    new_workout = workouts(
+        Name="Temporary Workout",
+        Description="Temporary workout for section test"
+    )
+    session.add(new_workout)
+    session.commit()
+    session.refresh(new_workout)
+    
+    new_workout_section = workout_sections(
+        WOID=new_workout.ID,
+        SectionName="Test warm up",
+        SectionOrder=4
+    )
+    
+    session.add(new_workout_section)
+    session.commit()
+    session.refresh(new_workout_section)
+    
+    assert new_workout_section.ID is not None
+    assert new_workout_section.WOID == new_workout.ID #tests valid foreign key insertion
+    assert new_workout_section.SectionName == "Test warm up"
+    assert new_workout_section.SectionOrder == 4
+    
+    session.delete(new_workout_section)
+    session.delete(new_workout)
+    session.commit()
+    session.close()
+    
+def test_insert_invalid_workout_sections():
+    session = SessionLocal()
+    
+    new_workout = workouts(
+        Name="Temporary Workout",
+        Description="Temporary workout for section test"
+    )
+    session.add(new_workout)
+    session.commit()
+    session.refresh(new_workout)
+    
+    bad_workout_section = workout_sections(
+        WOID=new_workout.ID,
+        SectionName=None,
+        SectionOrder=4
+    )
+    
+    session.add(bad_workout_section)
+    
+    with pytest.raises(IntegrityError):
+        session.commit()
+        
+    session.rollback()
+    session.delete(new_workout)
+    session.commit()
+    session.close()
+    
+def test_insert_invalid_foreign_key_workout_section():
+    session = SessionLocal()
+    
+    bad_workout_section = workout_sections(
+        WOID=9999,
+        SectionName="Bad Section",
+        SectionOrder=1
+    )
+    
+    session.add(bad_workout_section)
+    
+    with pytest.raises(IntegrityError):
+        session.commit()
+        
+    session.rollback()
+    session.close()
+    
