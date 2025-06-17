bd33be5b6eb75d689d32bdc38be0442dad5152c9
aimei60 <lizali26@outlook.com>
2025-06-17 08:16:15 +0100

    I have restructured my tables as i realised the user does not need every single detail in models tables. i want to make it simple and relevant for them. i have also structured my tables based on python schema rules.

--- backend/application/schemas.py
+++ backend/application/schemas.py
@@ -1,12 +1,29 @@
 from pydantic import BaseModel, Field
 
-class WorkoutBase(BaseModel):
-    Name: str = Field(..., max_length=50) #Name in workouts has 50 max characters
-    Description: str
+#schema for workout routine table
+
+class WorkoutRoutineRead(BaseModel): #user output - returns the below fields
+    Name: str = Field(..., max_length=50)
+    RepsDuration: str = Field(..., max_length=20)
+    RoutineDescription: str = Field(..., max_length=225)
+    ExerciseOrder: int
     
-class WorkoutRead(WorkoutBase):
-    ID: int
-    
-    class config:
+    class Config:
         orm_mode = True
         
+#schema for workout section table
+class WorkoutSection(BaseModel): #user output returns section name
+    SectionName: str = Field(..., max_length=50)
+    
+    class Config:
+        orm_mode = True
+    
+#schema for workouts table
+class WorkoutBase(BaseModel): #user input. User can type which workout they want
+    Name: str = Field(..., max_length=50) #user input. Name in workouts has 50 max characters
+    
+class WorkoutRead(WorkoutBase): #user output returns the decscription of the workout
+    Description: str
+    
+    class Config:
+        orm_mode = True
