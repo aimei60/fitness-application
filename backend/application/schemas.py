6cec25e0e45e37629836bdaffe6076a02bbedb7c
aimei60 <lizali26@outlook.com>
2025-06-20 10:13:54 +0100

    Added a schema for the user to update their password and also added my schema for user request, where users optionally can request for different workouts.

--- backend/application/schemas.py
+++ backend/application/schemas.py
@@ -1,5 +1,6 @@
 from pydantic import BaseModel, Field, EmailStr
-from typing import List
+from typing import List, Optional
+from enum import Enum
 
 #schema for workout routine table
 class WorkoutRoutineRead(BaseModel): #user output - returns the below fields
@@ -45,3 +46,21 @@ class UserRead(BaseModel):#having this incase I want these details to be shown i
 class UserPasswordChange(BaseModel): #user input to change password
     current_password: str
     new_password: str = Field(..., min_length=8)
+    
+#schema for user request - the below schema is optional for the user and if the user wants to request a more tailored or reviewed workout plan workout
+class RequestTypeEnum(str, Enum):
+    new_workout = "new_workout" #User wants a new plan 
+    repeat_workout = "repeat_workout" #User wants to re-do their last plan
+    upgrade_level = "upgrade_level" #User thinks they’ve progressed to next level e.g. beginner to intermediate
+    rehab_plan = "rehab_plan" #Recovery/injury-related requests
+    
+class UserWorkoutRequestCreate(BaseModel): #user input
+    RequestType: RequestTypeEnum
+    WorkoutID: Optional[int] = None
+    
+class UserWorkoutRequestRead(BaseModel): #user output
+    RequestType: RequestTypeEnum
+    Status: str
+
+    class Config:
+        orm_mode = True
