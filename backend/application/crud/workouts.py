cd1b87398f43552bd70715503907dbea0af9a0d8
aimei60 <lizali26@outlook.com>
2025-06-23 17:10:13 +0100

    I had to change the second function as what I was requesting was not returning the entire workout routine for the user

--- backend/application/crud/workouts.py
+++ backend/application/crud/workouts.py
@@ -1,11 +1,10 @@
-from sqlalchemy.orm import Session
+from sqlalchemy.orm import Session, joinedload
 from models import workouts
 
 #shows the user all available workout options
 def get_all_workouts(db: Session):
     return db.query(workouts).all()
 
-#searches for one specific workout by the workout name - user selects workout
-def get_workout_name(db: Session, name: str):
-    return db.query(workouts).filter(workouts.Name == name).first()
-
+#returns the workout name, description, section and the workout routine for the user
+def get_workout_with_sections_and_routines(db: Session, name: str):
+    return (db.query(workouts).options(joinedload(workouts.T1).joinedload("T3")).filter(workouts.Name == name).first())
