c7d823ebc63f25325567f31a18ae1cc377a74d88
aimei60 <lizali26@outlook.com>
2025-07-05 17:37:42 +0100

    minor errors

--- backend/application/crud/workouts.py
+++ backend/application/crud/workouts.py
@@ -1,5 +1,5 @@
 from sqlalchemy.orm import Session, joinedload
-from backend.application.models import workouts
+from backend.application.models import workouts, workout_sections
 
 #shows the user all available workout options
 def get_all_workouts(db: Session):
@@ -7,4 +7,4 @@ def get_all_workouts(db: Session):
 
 #returns the workout name, description, section and the workout routine for the user
 def get_workout_with_sections_and_routines(db: Session, name: str):
-    return (db.query(workouts).options(joinedload(workouts.T1).joinedload("T3")).filter(workouts.Name == name).first())
+    return (db.query(workouts).options(joinedload(workouts.T1).joinedload(workout_sections.T3)).filter(workouts.Name == name).first())
--- backend/application/routers/workouts.py
+++ backend/application/routers/workouts.py
@@ -1,8 +1,8 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from backend.application.database import get_db
-from backend.application.crud.workouts import get_all_workouts
-from backend.application.schemas import WorkoutSimpleRead
+from backend.application.crud.workouts import get_all_workouts, get_workout_with_sections_and_routines
+from backend.application.schemas import WorkoutSimpleRead, WorkoutRead
 from typing import List
 
 router = APIRouter()
@@ -10,3 +10,11 @@ router = APIRouter()
 @router.get("/workouts", response_model=List[WorkoutSimpleRead])
 def read_workouts(db: Session = Depends(get_db)):
     return get_all_workouts(db)
+
+@router.get("/workouts/{name}", response_model=WorkoutRead)
+def read_chosen_workout(name: str, db: Session = Depends(get_db)):
+    workout = get_workout_with_sections_and_routines(db, name)
+    if workout is None:
+        raise HTTPException(status_code=404, detail="Workout not found")
+    return workout
+    
