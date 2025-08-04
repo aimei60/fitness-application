7f0c89a80464abdfffa38a901807e8900040d9d3
aimei60 <lizali26@outlook.com>
2025-08-04 07:47:21 +0100

    Finished testing my crud workouts file and added the necessary documentation for this

--- backend/tests/test_crud.py
+++ backend/tests/test_crud.py
@@ -1,30 +1,57 @@
 import pytest
-from backend.sqlalchemy_test.app.test_database import Base, engine, SessionLocal
-from backend.application.models import workouts, workout_sections, workoutRoutine
-from backend.application.crud.workouts import get_all_workouts
+from backend.sqlalchemy_test.app.test_database import SessionLocal
+from backend.application.models import workouts
+from backend.application.crud.workouts import get_all_workouts, get_workout_with_sections_and_routines
+from backend.tests.test_utils import insert_sample_entire_workout
 
-"""def insert_test_workout(db):
+#insert workout sample data into the workouts table in the test db
+def insert_test_workout(db):
     workout = workouts(Name="Test 1", Description="Test Description 1")
     db.add(workout)
     db.commit()
     db.refresh(workout)
     return workout
 
+#checks an inserted workout is present when the list of all workouts is shown
 def test_workout_retrieval():
     db = SessionLocal()
-    try:
-        inserted = insert_test_workout(db)
-        
-        results = get_all_workouts(db)
-
-        names = []
-        for w in results:
-            names.append(w.Name)
-        assert inserted.Name in names
-    finally:
-        db.close()"""
-        
     
+    inserted = insert_test_workout(db)
+    results = get_all_workouts(db)
+
+    names = []
+    for w in results:
+        names.append(w.Name)
+    assert inserted.Name in names
+    
+    db.delete(inserted)
+    db.commit()
+    db.close()
+
+#inserts entire sample workout and check its been entered correctly and retrieved correctly
+def test_workout_with_sections_and_routines():
+    db = SessionLocal()
+    workout, section, routine = insert_sample_entire_workout(db)
+        
+    result = get_workout_with_sections_and_routines(db, "Test Workout")
+        
+    assert result is not None
+    assert result.Name == workout.Name
+    assert len(result.Sections) == 1
+    assert result.Sections[0].SectionName == section.SectionName # use 0 as result.Sections is a list
+    assert len(result.Sections[0].Routines) == 1
+    assert result.Sections[0].Routines[0].Name == routine.Name
+    
+    db.delete(routine)
+    db.delete(section)
+    db.delete(workout)
+    db.commit()
+    db.close()
+        
+        
+
+        
+
     
     
     
--- backend/tests/test_utils.py
+++ backend/tests/test_utils.py
@@ -4,22 +4,20 @@ print("PYTHONPATH:", sys.path)
 from backend.sqlalchemy_test.app.test_database import SessionLocal
 from backend.application.models import workouts, workout_sections, workoutRoutine
 
+#inserts workout, workout section and workout routine in the test db
 def insert_sample_entire_workout(db):
-    #insert workout 
     workout = workouts(Name="Test Workout", Description="Test Description")
     db.add(workout)
     db.commit()
     db.refresh(workout)
     
-    #insert workout section
     section = workout_sections(SectionName = "Test Section",
                                SectionOrder=1,
                                WOID=workout.ID)
     db.add(section)
     db.commit()
     db.refresh(section)
-    
-    #insert workout routine
+
     routine = workoutRoutine(
         Name="Test Routine",
         RepsDuration="10 reps",
@@ -33,15 +31,6 @@ def insert_sample_entire_workout(db):
     
     return workout, section, routine
     
-    
-if __name__ == "__main__":
-    db = SessionLocal()
-    try:
-        #insert_sample_entire_workout(db)
-        print("✅ Inserted test data from test_utils.py")
-    finally:
-        db.close()
-
 
 
 
