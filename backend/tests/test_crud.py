ebaa7d55d7353f36d8d542bde0abd5a2104a91cc
aimei60 <lizali26@outlook.com>
2025-08-07 08:44:38 +0100

    finished testing the user crud functions

--- backend/tests/test_crud.py
+++ backend/tests/test_crud.py
@@ -1,21 +1,25 @@
 import pytest
+from passlib.context import CryptContext
+from unittest.mock import MagicMock
+from fastapi import status, HTTPException
 from backend.sqlalchemy_test.app.test_database import SessionLocal
 from backend.application.models import workouts, User
 from backend.application.crud.workouts import get_all_workouts, get_workout_with_sections_and_routines
 from backend.tests.test_utils import insert_sample_entire_workout, insert_sample_user
-from backend.application.crud.user import create_user, get_user_email_and_active_status
-from backend.application.schemas import UserCreate
-from backend.application.utilities.security import verify_password
+from backend.application.crud.user import create_user, get_user_email_and_active_status, update_user_password
+from backend.application.schemas import UserCreate, UserPasswordChange
+from backend.application.utilities.security import get_password_hash, verify_password
+
 
 #insert workout sample data into the workouts table in the test db
-"""def insert_test_workout(db):
+def insert_test_workout(db):
     workout = workouts(Name="Test 1", Description="Test Description 1")
     db.add(workout)
     db.commit()
     db.refresh(workout)
-    return workout"""
+    return workout
 
-"""#checks an inserted workout is present when the list of all workouts is shown
+#checks an inserted workout is present when the list of all workouts is shown
 def test_workout_retrieval():
     db = SessionLocal()
     
@@ -29,7 +33,7 @@ def test_workout_retrieval():
     
     db.delete(inserted)
     db.commit()
-    db.close()"""
+    db.close()
 
 #inserts entire sample workout and check its been entered correctly and retrieved correctly
 def test_workout_with_sections_and_routines():
@@ -50,7 +54,8 @@ def test_workout_with_sections_and_routines():
     db.delete(workout)
     db.commit()
     db.close()
-    
+ 
+#tests the creation of a new user and checks its email and password is correct    
 def test_create_user():
     db = SessionLocal()
     
@@ -64,7 +69,8 @@ def test_create_user():
     db.delete(create_test_user)
     db.commit()
     db.close()
-    
+
+#tests that the users details e.g. email and active status is retrieved correctly    
 def test_retrieve_user_details():
     db = SessionLocal()
     
@@ -77,7 +83,43 @@ def test_retrieve_user_details():
     db.delete(user)
     db.commit()
     db.close()
+
+#tests the user updating their password correctly using mocking    
+def test_update_user_password_success():
+    original_test_password = "oldpassword1"
+    hashed_password = get_password_hash(original_test_password)
     
+    mock_user = User()
+    mock_user.HashedPassword = hashed_password
+    mock_db = MagicMock()
+    
+    user_test_input = UserPasswordChange(current_password="oldpassword1", new_password="newpassword2")
+    response = update_user_password(db=mock_db, user_input=user_test_input, current_user=mock_user)
+    
+    assert verify_password("newpassword2", mock_user.HashedPassword)
+    mock_db.commit.assert_called_once()
+    
+    assert response == {"message": "Password updated successfully"}
+
+#tests the correct errors are raised when the wrong password is entered to update the user's password    
+def test_update_user_password_failure():
+    hashed_password = get_password_hash("correctpassword")
+    mock_user = User()
+    mock_user.HashedPassword = hashed_password
+    
+    mock_db = MagicMock()
+    
+    user_test_input = UserPasswordChange(current_password="wrongpassword", new_password="Bluesky1")
+    
+    with pytest.raises(HTTPException) as exc_info:
+        update_user_password(db=mock_db, user_input=user_test_input, current_user=mock_user)
+        
+    assert exc_info.value.status_code == status.HTTP_403_FORBIDDEN
+
+        
+     
+    
+
         
 
         
