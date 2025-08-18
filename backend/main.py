c4b3e5b6dab5881deb66fe0df39400b60307550d
aimei60 <lizali26@outlook.com>
2025-08-18 11:22:59 +0100

    added my cors code into main.py in prep for react

--- backend/main.py
+++ backend/main.py
@@ -3,8 +3,22 @@ print("sys.path =", sys.path)
 
 from fastapi import FastAPI
 from .application.routers import workouts, user, user_request, auth, profile
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()  
+
+origins = [
+    "http://localhost:5173", #react  
+    "http://127.0.0.1:8000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=False,  
+    allow_methods=["*"],
+    allow_headers=["*"],    
+)
 
 app.include_router(workouts.router)
 app.include_router(user.router)
