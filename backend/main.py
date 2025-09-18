02b75a91698f2b9081ce5399e8d43ed8839d1b1c
aimei60 <lizali26@outlook.com>
2025-09-18 10:17:48 +0100

    set up my dockerfile and dockerignore file. Also added some documentation and finalised my main.py file.

--- backend/main.py
+++ backend/main.py
@@ -1,20 +1,21 @@
-import sys
-print("sys.path =", sys.path)
+#initialises FastAPI, configures CORS, registers routers and includes 2 routes
 
 from fastapi import FastAPI
 from .application.routers import workouts, user, user_request, auth, profile
 from fastapi.middleware.cors import CORSMiddleware
+import os
 
 app = FastAPI()  
 
 origins = [
-    "http://localhost:5173",
-    "http://127.0.0.1:5173", #react  
+    os.getenv("FRONTEND_PREVIEW", "http://localhost:5173"), 
+    os.getenv("FRONTEND_PRODUCTION", "https://frontend.vercel.app"),
+    os.getenv("DOMAIN", "https://domainname.com"),
 ]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=[o for o in origins if o], #cleans up urls by removing anything empty None or ""
     allow_credentials=False,  #change to true if planning to use cookies
     allow_methods=["*"],
     allow_headers=["*"],    
@@ -29,3 +30,7 @@ app.include_router(profile.router)
 @app.get("/")
 def root():
     return {"message": "Welcome to the Fitness Application"}
+
+@app.get("/health") #health check
+def health(): 
+    return {"ok": True}
