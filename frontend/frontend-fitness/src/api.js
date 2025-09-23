b20a949932be3937a06c92e580f543ab66bb5696
aimei60 <lizali26@outlook.com>
2025-09-23 19:50:04 +0100

    installed axios agaian and fixed the use of vite_api_url

--- frontend/frontend-fitness/src/api.js
+++ frontend/frontend-fitness/src/api.js
@@ -1,17 +1,19 @@
 //handles authentication tokens: stores them, loads them when the application starts and makes sure all axios requests are authenticated automatically
 import axios from "axios";
 
-//creates axios to make http requests. all requests go to backend --> vite_api_base or http://localhost:8000
+// create axios instance with correct base URL
 export const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE || "http://localhost:8000",
-  withCredentials: false
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:8000",
+  withCredentials: false,
 });
 
-// checks if token was saved in local storage from a previous login and if found axios attaches it to every request and this makes sure the suer stays logged in if the page refreshes
+// load saved token from localStorage and attach to axios if found
 let token = localStorage.getItem("access_token");
-if (token) api.defaults.headers.common.Authorization = `Bearer ${token}`;
+if (token) {
+  api.defaults.headers.common.Authorization = `Bearer ${token}`;
+}
 
-//logging in/ out set token function. logging in: saves token to local storage and sets axios authorisation header. logging out: removes token from storage and stops sending it in requests
+// helper function to set or clear token
 export function setToken(t) {
   token = t;
   if (t) {
@@ -22,3 +24,4 @@ export function setToken(t) {
     delete api.defaults.headers.common.Authorization;
   }
 }
+
