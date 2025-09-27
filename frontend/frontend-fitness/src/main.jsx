d06e061013130765bf2e6a5a64042161717c78cc
aimei60 <lizali26@outlook.com>
2025-09-27 14:33:24 +0100

    updated my navbar.css as it was causing overflow on mobile. also reinforced some styling of global.css

--- frontend/frontend-fitness/src/main.jsx
+++ frontend/frontend-fitness/src/main.jsx
@@ -1,8 +1,9 @@
+import './css/global.css'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from "react-router-dom";
 import App from './App.jsx'
-import './css/global.css'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
